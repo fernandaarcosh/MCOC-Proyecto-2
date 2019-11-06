f2b2c9a779e3043131d4faaf92b43576632d789f
Fernanda Arcos Hernández <53495949+fernandaarcosh@users.noreply.github.com>
2019-11-05 22:19:02 -0300

    Update parametros.py

--- Entrega 6/parametros.py
+++ Entrega 6/parametros.py
@@ -1,10 +1,8 @@
 
 from matplotlib.pylab import *
+
 # Numero de Particulas a modelar
-
 Nparticulas =5
-
-# PARAMETROS
 
 #Unidades base son SI
 _m = 1.
@@ -15,37 +13,44 @@ _cm = 1e-2*_m
 _gr = 1e-3 * _kg
 _in = 2.54*_cm
 
+# Propiedades fisicas y geometricas de la/s particula/s:
 g = 9.81*_m/_s**2
 d = 0.15*_mm
 
 rho_agua = 1000.*_kg/(_m**3)
 rho_particula = 2650.*_kg/(_m**3)
 
-dt = 0.00001*_s    	# paso de tiempo 
-tmax = 0.05*_s  # tiempo maximo de simulacion
+# Datos de tiempo
+dt = 0.00001*_s  
+tmax = 0.05*_s  
 
-Cd = 0.47 # Para particula esferica
+# Constantes para fuerzas hidrodinamicas utilizadas:
+Cd = 0.47 
 Cm = 0.5
 CL = 0.2
 Rp = 73.
+
 ustar = 0.14
 tau_star = 0.067  
+
 R = (rho_particula/rho_agua - 1)
 alpha = 1/(1 + R + Cm)
 
-ihat = array([1,0])
-jhat = array([0,1])
 norm=lambda v: sqrt(dot(v,v))
+
 tau_cr = 0.22*Rp**(-0.6)+0.06*10**(-7*Rp**(-0.6))   # tau critico
 ustar = sqrt(tau_star * g * Rp * d)
+
+# Coordenadas vectoriales, eje x e y:
+ihat = array([1,0])
+jhat = array([0,1])
 
 print "tau_star=",tau_star
 print "tau_cr=",tau_cr
 print "tau_star/tau_cr=",tau_star/tau_cr
 print "ustar=",ustar
 
-
-# FUNCIONES
+# Definicion de la velocidad de el fluido
 def velocity_field(x):
 	z = x[1] /d
 
